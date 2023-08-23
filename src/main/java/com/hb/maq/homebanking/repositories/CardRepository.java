277f3f95a45040d14afa46561d3911c951625ac4
Miguel Ángel Quinteros <maq.miguel.quinteros@gmail.com>
2023-08-23 17:27:57 -0300

    Corrección de Task06

--- src/main/java/com/hb/maq/homebanking/repositories/CardRepository.java
+++ src/main/java/com/hb/maq/homebanking/repositories/CardRepository.java
@@ -2,6 +2,8 @@ package com.hb.maq.homebanking.repositories;
 
 import com.hb.maq.homebanking.models.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CardRepository extends JpaRepository <Card, Long> {
 }
